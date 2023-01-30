a664eac2b1fffad312abb21460f74c22d5ce579d
Nijumdip <nijumdip019911@gmail.com>
2023-01-30 08:23:29 +0600

    navbar responsiveness done alhamdulillah

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Home = () => {
-  return (
-    <Wrapper>Home</Wrapper>
-  );
+    return <Wrapper>Home</Wrapper>;
 };
 
 const Wrapper = styled.section`
-  background-color: ${({ theme }) => theme.colors.bg};
+    height: 100vh;
+    background-color: ${({ theme }) => theme.colors.bg};
 `;
 
 export default Home;
