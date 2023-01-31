4745bdb79768d000167c1578de3680eb43112ab1
Nijumdip <nijumdip019911@gmail.com>
2023-01-31 17:28:48 +0600

    added error page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const ErrorPage = () => {
   return (
     <div>ErrorPage</div>
   )
-}
+};
+
+const Wrapper = styled.section`
+  .container {
+    padding: 9rem 0;
+    text-align: center;
+    h2 {
+      font-size: 10rem;
+    }
+    h3 {
+      font-size: 4.2rem;
+    }
+    p {
+      margin: 2rem 0;
+    }
+  }
+`;
 
 export default ErrorPage
