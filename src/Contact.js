74d7defe879ba94fb60a9acc3b67e0869db9ddfc
Nijumdip <nijumdip019911@gmail.com>
2023-01-31 12:36:12 +0600

    added formspree library

--- src/Contact.js
+++ src/Contact.js
@@ -35,7 +35,53 @@ const Contact = () => {
 
     return (
         <Wrapper>
-            <h1>Contact page</h1>
+            <h2 className="common-heading">Contact page</h2>
+
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3156.8064539258976!2d90.42411356512496!3d23.91708984851268!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c52a982608df%3A0x45b8d1544d7d6427!2sSooner%20Siri%20park%20opposite!5e0!3m2!1sen!2sbd!4v1675126771827!5m2!1sen!2sbd"
+                width="100%"
+                height="450"
+                style={{ border: "0" }}
+                allowFullscreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title=" "
+            ></iframe>
+
+            <div className="container">
+                <div className="contact-form">
+                    <form action="" method="POST">
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="username"
+                            value=""
+                            required
+                            autoComplete="off"
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value=""
+                            required
+                            autoComplete="off"
+                        />
+
+                        <textarea
+                            name="message"
+                            id=""
+                            cols="30"
+                            rows="10"
+                            placeholder="Enter your message"
+                            required
+                            autoComplete="off"
+                        ></textarea>
+
+                        <input type="submit" value="Send"/>
+                    </form>
+                </div>
+            </div>
         </Wrapper>
     );
 };
