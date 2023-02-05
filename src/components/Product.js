bd5231d4ee00256356cc457a164524d9838067cc
Nijumdip <nijumdip019911@gmail.com>
2023-02-05 14:24:05 +0600

    get feature product data

---
+++ src/components/Product.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Product = (curElem) => {
+    const { id, name, image, price, category }=curElem;
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
