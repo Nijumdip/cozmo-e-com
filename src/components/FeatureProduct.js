db47f7963e2f93f01f6e87bc2c5f9ed8c42884d6
Nijumdip <nijumdip019911@gmail.com>
2023-02-05 10:45:15 +0600

    added feature product page

--- src/components/FeatureProduct.js
+++ src/components/FeatureProduct.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useProductContext } from '../context/productcontex'
+
 
 const FeatureProduct = () => {
+    const {isLoading, featureProducts} = useProductContext()
   return (
     <div>FeatureProduct</div>
   )
