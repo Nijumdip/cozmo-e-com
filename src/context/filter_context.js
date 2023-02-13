10ccdc97e93384df92dd3ac16620f10910724bf4
Nijumdip <nijumdip019911@gmail.com>
2023-02-13 21:17:25 +0600

    added filterContext page

---
+++ src/context/filter_context.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { createContext } from 'react'
+
+const FilterContext = createContext();
+
+export const FilterContextProvider = ({ children }) => {
+    return <FilterContext.Provider value={{ ...state }}>{children}</FilterContext.Provider>
+};
+
+export const useFilterContext = () => {
+    return useContext(FilterContext)
+}
