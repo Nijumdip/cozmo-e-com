f4017cb982a15e4488a8f2f01944eba9b2e21d7e
Nijumdip <nijumdip019911@gmail.com>
2023-02-14 21:56:09 +0600

    added grid view page

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import styled from 'styled-components';
+import Product from './Product';
 
-const GridView = () => {
+const GridView = ({ products }) => {
+    // console.log(products);
     return (
-        <Wrapper>GridView</Wrapper>
+        <Wrapper className="section">
+            <div className="container grid grid-three-column">
+                {
+                    products.map((curElem) => {
+                        return <Product key={curElem.id} {...curElem} />
+                    })
+                }
+            </div>
+        </Wrapper>
     );
 };
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,7 @@ import GridView from './GridView';
 const ProductList = () => {
 
   const { filter_products, grid_view } = useFilterContext();
-    console.log(filter_products);
+    // console.log(filter_products);
 
   if (grid_view === true) {
     return <GridView products={filter_products} />
