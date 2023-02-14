28c18a6857d0cad4f8c2e760e10b807efcf92373
Nijumdip <nijumdip019911@gmail.com>
2023-02-14 21:19:14 +0600

    added filterReducer page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useFilterContext } from '../context/filter_context';
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
+
+  const { filter_products, setGridView } = useFilterContext();
+    console.log(filter_products);
+
+  if (setGridView === true) {
+    return <GridView products={filter_products} />
+  };
+  
 }
 
 export default ProductList
