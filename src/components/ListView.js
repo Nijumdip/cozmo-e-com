086bd4db89bc8672f1f5cad05812a70d59245ff0
Nijumdip <nijumdip019911@gmail.com>
2023-02-15 21:56:09 +0600

    added list view page

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -11,23 +11,7 @@ const ListView = ({ products }) => {
                 {products.map((curElem) => {
                     const { id, name, image, price, description } = curElem;
                     return (
-                        <div className="card grid grid-two-column">
-                            <figure>
-                                <img src={image} alt={name} />
-                            </figure>
-
-                            <div className="card-data">
-                                <h3>{name}</h3>
-                                <p>
-                                    <FormatPrice price={price} />
-                                </p>
-                                <p>{description.slice(0, 90)}...</p>
-
-                                <NavLink to={`/singleProduct/${id}`} className="btn-main">
-                                    <Button className="btn">Read More</Button>
-                                </NavLink>
-                            </div>
-                        </div>
+                        
                     );
                 })}
             </div>
