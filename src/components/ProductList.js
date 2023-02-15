cbd925b39984fa87acfdeab5fc2bde5a85d941c7
Nijumdip <nijumdip019911@gmail.com>
2023-02-15 20:12:39 +0600

    added list view page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useFilterContext } from '../context/filter_context';
 import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
 
   const { filter_products, grid_view } = useFilterContext();
+  
     // console.log(filter_products);
 
   if (grid_view === true) {
     return <GridView products={filter_products} />
   };
-  // if (grid_view === true) {
-  //   return <GridView products={filter_products} />
-  // };
+  if (grid_view === true) {
+    return <ListView products={filter_products} />
+  };
 
 }
 
