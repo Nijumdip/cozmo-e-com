066dd920c0de415fac4dde365662b9448e78ae21
Nijumdip <nijumdip019911@gmail.com>
2023-02-18 09:07:04 +0600

    added dropdown

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -4,7 +4,7 @@ import { BsFillGridFill, BsList } from "react-icons/bs";
 import { useFilterContext } from "../context/filter_context";
 
 const Sort = () => {
-    const { grid_view, setGridView, setListView, filter_products } = useFilterContext();
+    const { grid_view, setGridView, setListView, filter_products, sorting } = useFilterContext();
 
     return (
         <Wrapper className="sort-section">
@@ -29,7 +29,25 @@ const Sort = () => {
             <div className="product-data">{filter_products.length} Product available</div>
 
             {/* 3rd column  */}
-            <div className="sort-selection">dropdown</div>
+            <div className="sort-selection">
+                <form action="#">
+                    <label htmlFor="sort"></label>
+                    <select
+                        className="sort-selection--style"
+                        name="sort"
+                        id="sort"
+                        onClick={sorting}
+                    >
+                        <option value="lowest">Price(lowest)</option>
+                        <option value="#" disabled></option>
+                        <option value="highest">Price(highest)</option>
+                        <option value="#" disabled></option>
+                        <option value="a-z">Price(a-z)</option>
+                        <option value="#" disabled></option>
+                        <option value="z-a">Price(z-a)</option>
+                    </select>
+                </form>
+            </div>
         </Wrapper>
     );
 };
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,38 +1,42 @@
-import { useContext } from 'react';
-import { useEffect } from 'react';
-import { useReducer } from 'react';
-import { createContext } from 'react'
-import reducer from '../reducer/FilterReducer';
-import { useProductContext } from './productcontex';
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useReducer } from "react";
+import { createContext } from "react";
+import reducer from "../reducer/FilterReducer";
+import { useProductContext } from "./productcontex";
 const FilterContext = createContext();
 
 const initialState = {
     filter_products: [],
     all_products: [],
     grid_view: true,
-}
+};
 
 export const FilterContextProvider = ({ children }) => {
-
     const { products } = useProductContext();
     // console.log(products);
 
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const setGridView = () => {
-        dispatch({ type: "SET_GRID_VIEW" })
+        dispatch({ type: "SET_GRID_VIEW" });
     };
     const setListView = () => {
-        dispatch({ type: "SET_LIST_VIEW" })
+        dispatch({ type: "SET_LIST_VIEW" });
     };
+    const sorting = () => {};
 
     useEffect(() => {
-        dispatch({type:"LOAD_FILTER_PRODUCTS", payload:products})
-    },[products])
+        dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
+    }, [products]);
 
-    return <FilterContext.Provider value={{ ...state, setGridView, setListView }}>{children}</FilterContext.Provider>
+    return (
+        <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting }}>
+            {children}
+        </FilterContext.Provider>
+    );
 };
 
 export const useFilterContext = () => {
-    return useContext(FilterContext)
-}
+    return useContext(FilterContext);
+};
