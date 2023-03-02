5cd1309619ef1de6884c6669af7e830b3911244e
Nijumdip <nijumdip019911@gmail.com>
2023-03-02 23:01:22 +0600

    alhamdullah done filters

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -41,7 +41,6 @@ const AddToCart = ({ product }) => {
             <CartAmountToggle amount={amount} setDecrease={setDecrease} setIncrease={setIncrease} />
 
             <NavLink to="/cart">
-                
                 <Button>Add To Cart</Button>
             </NavLink>
         </Wrapper>
