105759b0654574b1464c1a624064f3026b20bbe9
Nijumdip <nijumdip019911@gmail.com>
2023-03-05 22:19:29 +0600

    add cartItem

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -17,9 +17,7 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
 
-  const removeItem = (id) => {
-    dispatch({ type: "REMOVE_ITEM", payload: id });
-  };
+  
 
 
   return (
