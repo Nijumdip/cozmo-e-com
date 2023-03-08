c9852b2637e0d4cf540294b08c6d7c409b5ce584
Nijumdip <nijumdip019911@gmail.com>
2023-03-08 20:39:22 +0600

    add local stroage

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -13,7 +13,6 @@ const cartReducer = (state, action) => {
             let newAmount = curElem.amount + amount;
             if (newAmount >= curElem.max) {
               newAmount = curElem.max;
-              
             }
             return {
               ...curElem,
