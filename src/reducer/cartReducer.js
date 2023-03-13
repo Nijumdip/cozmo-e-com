8c88a62a0842e836d8424dee54eb1ba91761f6ee
Nijumdip <nijumdip019911@gmail.com>
2023-03-13 20:34:51 +0600

    add decrease function

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -51,7 +51,13 @@ const cartReducer = (state, action) => {
     })
   }
 
-    
+    if (action.type === "REMOVE_ITEM") {
+        let updatedCart = state.cart.filter((curItem) => curItem.id !== action.payload);
+        return {
+            ...state,
+            cart: updatedCart,
+        };
+    }
 
     if (action.type === "CLEAR_CART") {
         return {
